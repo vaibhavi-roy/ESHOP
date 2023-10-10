66e54a81eeb40ba7fdd1e198d53aac00d5980ebd
Vaibhavi Roy <vaibhaviroy2002@gmail.com>
2023-10-10 15:53:40 +0530

    Authentication

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,26 @@
-import express from "express";
+import express from 'express';
 import dotenv from 'dotenv';
-import connectDB from "./config/db.js";
-import productRoutes from "./routes/productRoutes.js"
-import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
-// import { createProxyMiddleware } from "http-proxy-middleware";
-
-const port = process.env.PORT || 5000;
+import cookieParser from 'cookie-parser';
 dotenv.config();
-
-connectDB(); // connect to database
-
+import connectDB from './config/db.js';
+import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+const port = process.env.PORT || 5000;
+connectDB();
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 app.get('/', (req, res) => {
     res.send('API is running...');
 });
-
-app.use('/api/products', productRoutes);
-
 app.use(notFound);
 app.use(errorHandler);
-
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, () =>
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`)
+);
