de91bd723a5a7e07eeca8241277f2d840e8c0de1
Vaibhavi Roy <vaibhaviroy2002@gmail.com>
2023-12-20 13:19:23 +0530

    Reviews

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,9 +1,10 @@
 import { useParams, useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import { Form, Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { toast } from 'react-toastify';
 import Rating from '../components/Rating';
-import { useGetProductDetailsQuery } from '../slices/ProductsApiSlice';
+import { useGetProductDetailsQuery, useCreateReviewMutation } from '../slices/ProductsApiSlice';
 import Loader from '../components/Loader';
 import Message from '../components/Message';
 import { useState } from 'react';
@@ -17,15 +18,38 @@ const ProductScreen = () => {
     const navigate = useNavigate();
 
     const [qty, setQty] = useState(1);
+    const [rating, setRating] = useState(0);
+    const [comment, setComment] = useState('');
 
 
+    const { data: product, isLoading, refetch, error } = useGetProductDetailsQuery(productId);
 
-    const { data: product, isLoading, error } = useGetProductDetailsQuery(productId);
+    const [createReview, { isLoading: loadingProductReview }] = useCreateReviewMutation();
+
+    const { userInfo } = useSelector((state) => state.auth);
 
     const addToCartHandler = () => {
         dispatch(addToCart({ ...product, qty })); //items visible in local storage too
         navigate('/cart');
     }
+
+    const submitHandler = async (e) => {
+        e.preventDefault();
+        try {
+            await createReview({
+                productId,
+                rating,
+                comment,
+            }).unwrap();
+            refetch();
+            toast.success('Review Submitted');
+            setRating(0);
+            setComment('');
+        } catch (err) {
+            toast.error(err?.data?.message || err.error);
+        }
+    };
+
 
     return (
         <>
@@ -111,6 +135,69 @@ const ProductScreen = () => {
                                 </ListGroup.Item>
                             </ListGroup>
                         </Card>
+                    </Col>
+                </Row>
+                <Row className='review'>
+                    <Col md={6}>
+                        <h2>Reviews</h2>
+                        {product.reviews.length === 0 && <Message>No Reviews</Message>}
+                        <ListGroup variant='flush'>
+                            {product.reviews.map((review) => (
+                                <ListGroup.Item key={review._id}>
+                                    <strong>{review.name}</strong>
+                                    <Rating value={review.rating} />
+                                    <p>{review.createdAt.substring(0, 10)}</p>
+                                    <p>{review.comment}</p>
+                                </ListGroup.Item>
+                            ))}
+                            <ListGroup.Item>
+                                <h2>Write a Customer Review</h2>
+
+                                {loadingProductReview && <Loader />}
+
+                                {userInfo ? (
+                                    <Form onSubmit={submitHandler}>
+                                        <Form.Group className='my-2' controlId='rating'>
+                                            <Form.Label>Rating</Form.Label>
+                                            <Form.Control
+                                                as='select'
+                                                required
+                                                value={rating}
+                                                onChange={(e) => setRating(e.target.value)}
+                                            >
+                                                <option value=''>Select...</option>
+                                                <option value='1'>1 - Poor</option>
+                                                <option value='2'>2 - Fair</option>
+                                                <option value='3'>3 - Good</option>
+                                                <option value='4'>4 - Very Good</option>
+                                                <option value='5'>5 - Excellent</option>
+                                            </Form.Control>
+                                        </Form.Group>
+                                        <Form.Group className='my-2' controlId='comment'>
+                                            <Form.Label>Comment</Form.Label>
+                                            <Form.Control
+                                                as='textarea'
+                                                row='3'
+                                                required
+                                                value={comment}
+                                                onChange={(e) => setComment(e.target.value)}
+                                            ></Form.Control>
+                                        </Form.Group>
+                                        <Button
+                                            disabled={loadingProductReview}
+                                            type='submit'
+                                            variant='primary'
+                                        >
+                                            Submit
+                                        </Button>
+                                    </Form>
+                                ) : (
+                                    <Message>
+                                        Please <Link to='/login'>sign in</Link> to write a review
+                                    </Message>
+                                )}
+                            </ListGroup.Item>
+                        </ListGroup>
                     </Col>
                 </Row>
             </>)}
--- frontend/src/slices/ProductsApiSlice.js
+++ frontend/src/slices/ProductsApiSlice.js
@@ -44,8 +44,16 @@ export const productsApiSlice = apiSlice.injectEndpoints({
                 method: 'DELETE',
             }),
         }),
+        createReview: builder.mutation({
+            query: (data) => ({
+                url: `${PRODUCTS_URL}/${data.productId}/reviews`,
+                method: 'POST',
+                body: data,
+            }),
+            invalidatesTags: ['Product'],
+        }),
     }),
 
 });
 
-export const { useGetProductsQuery, useGetProductDetailsQuery, useCreateProductMutation, useUpdateProductMutation, useUploadProductImageMutation, useDeleteProductMutation } = productsApiSlice; //convention to export query
+export const { useGetProductsQuery, useGetProductDetailsQuery, useCreateProductMutation, useUpdateProductMutation, useUploadProductImageMutation, useDeleteProductMutation, useCreateReviewMutation } = productsApiSlice; //convention to export query,mutations
